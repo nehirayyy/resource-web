4f09326a3cdba79e3af466017b6ea950fd0a991e
nehirayyy <nehiray@ocadu.ca>
2024-02-16 09:40:53 -0500

    hover working
    you can now hover over the letters. I just started playing with shadow so I can get a glow during hover

--- wireframe/larp.js
+++ wireframe/larp.js
@@ -1,5 +1,41 @@
-function hoverGlow(){
-    var text = document.getElementById("L").style.backgroundColor = "red";
+function hoverGlowL(){
+    var text = document.getElementById("L").style.backgroundColor = "black";
 }
 
-onmouseover = "hoverGlow()";
+function removeGlowL(){
+    var text = document.getElementById("L").style.backgroundColor = "";
+}
+
+
+function hoverGlowA(){
+    var text = document.getElementById("A").style.backgroundColor = "black";
+}
+
+function removeGlowA(){
+    var text = document.getElementById("A").style.backgroundColor = "";
+}
+function hoverGlowR(){
+    var text = document.getElementById("R").style.backgroundColor = "black";
+}
+
+function removeGlowR(){
+    var text = document.getElementById("R").style.backgroundColor = "";
+}
+function hoverGlowP(){
+    var text = document.getElementById("P").style.backgroundColor = "black";
+}
+
+function removeGlowP(){
+    var text = document.getElementById("P").style.backgroundColor = "";
+}
+onmouseover = "hoverGlowL()";
+onmouseover = "removeGlowL()";
+
+onmouseover = "hoverGlowA()";
+onmouseover = "removeGlowA()";
+
+onmouseover = "hoverGlowR()";
+onmouseover = "removeGlowR()";
+
+onmouseover = "hoverGlowP()";
+onmouseover = "removeGlowP()";
